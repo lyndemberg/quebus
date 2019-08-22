3e3e0c7280c52cace1f35103823d31a243280e2b
Aguirre Sabino <aguirresabino187@gmail.com>
2019-08-22 17:45:58 -0300

    configurando cors middleware

---
+++ server/middlewares/cors.js
@@ -0,0 +1,9 @@
+const cors = require('cors');
+
+module.exports = api => {
+  api.use(cors({
+    origin: ['*'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+}
