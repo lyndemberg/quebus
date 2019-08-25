a5362fed75c0ce234b419e6ff1070a79187d7137
Aguirre Sabino <aguirresabino187@gmail.com>
2019-08-25 15:47:27 -0300

    adicionando lib http-status-codes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,5 @@
+const httpStatus = require('http-status-codes');
+
 module.exports = (api) => {
   const userService = api.services.user;
 
@@ -5,41 +7,49 @@ module.exports = (api) => {
     save: async (req, res) => {
       try {
         const data = await userService.save(req.body);
-        res.status(201).json({ message: 'OK', status: 201, data });
+        res.status(httpStatus.CREATED).json({ message: 'OK', status: httpStatus.CREATED, data });
       } catch (error) {
-        res.status(400).json({ message: error.message, status: 400, data: {} });
+        res.status(httpStatus.BAD_REQUEST).json({
+          message: error.message, status: httpStatus.BAD_REQUEST, data: {},
+        });
       }
     },
 
     findAll: async (req, res) => {
       const data = await userService.findAll();
-      res.status(200).json({ message: 'OK', status: 200, data });
+      res.status(httpStatus.OK).json({ message: 'OK', status: httpStatus.OK, data });
     },
 
     findById: async (req, res) => {
       try {
         const data = await userService.findById(req.params.id);
-        res.status(200).json({ message: 'OK', status: 200, data });
+        res.status(httpStatus.OK).json({ message: 'OK', status: httpStatus.OK, data });
       } catch (error) {
-        res.status(404).json({ message: error.message, status: 404, data: {} });
+        res.status(httpStatus.NOT_FOUND).json({
+          message: error.message, status: httpStatus.NOT_FOUND, data: {},
+        });
       }
     },
 
     update: async (req, res) => {
       try {
         const data = await userService.update(req.body);
-        res.status(200).json({ message: 'OK', status: 200, data });
+        res.status(httpStatus.OK).json({ message: 'OK', status: httpStatus.OK, data });
       } catch (error) {
-        res.status(400).json({ message: error.message, status: 400, data: {} });
+        res.status(httpStatus.BAD_REQUEST).json({
+          message: error.message, status: httpStatus.BAD_REQUEST, data: {},
+        });
       }
     },
 
     delete: async (req, res) => {
       try {
         const data = await userService.delete(req.params.id);
-        res.status(204);
+        res.status(httpStatus.NO_CONTENT);
       } catch (error) {
-        res.status(404).json({ message: error.message, status: 404, data: {} });
+        res.status(httpStatus.NOT_FOUND).json({
+          message: error.message, status: httpStatus.NOT_FOUND, data: {},
+        });
       }
     },
   };
