a3f85213ec070f6a0d0589234b15ff0a2b1a633d
lyndemberg <lyndembergbatista@outlook.com>
2019-08-28 16:24:16 -0300

    removendo app component e usando login component como boostrap do projeto

--- quebus-app/src/app/app.module.ts
+++ quebus-app/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { MaterializeModule } from 'angular2-materialize';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { MaterializeModule } from 'angular2-materialize';
     MaterializeModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [LoginComponent]
 })
 export class AppModule { }
