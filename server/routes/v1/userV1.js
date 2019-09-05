ca60f280c88777d00f5c9549ac2c556643ee9beb
Aguirre Sabino <aguirresabino187@gmail.com>
2019-09-04 22:14:22 -0300

    refatorando rotas em routes/v1/userV1.js

--- server/routes/v1/userV1.js
+++ server/routes/v1/userV1.js
@@ -1,13 +1,13 @@
 module.exports = (api) => {
   const userController = api.controllers.user;
-  const URL_BASE = '/quebus/v1';
+  const URL_BASE = '/quebus/v1/user';
 
-  api.route(`${URL_BASE}/user/auth`)
+  api.route(`${URL_BASE}/auth`)
     .post((req, res) => {
       userController.auth(req, res);
     });
 
-  api.route(`${URL_BASE}/user`)
+  api.route(`${URL_BASE}`)
     .get((req, res) => {
       userController.findAll(req, res);
     })
@@ -15,7 +15,7 @@ module.exports = (api) => {
       userController.save(req, res);
     });
 
-  api.route(`${URL_BASE}/user/:id`)
+  api.route(`${URL_BASE}/:id`)
     .get((req, res) => {
       userController.findById(req, res);
     })
