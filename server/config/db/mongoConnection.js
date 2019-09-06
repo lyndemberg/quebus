c70926f22ad0c4bdddd32301b594c9210c1961f4
Aguirre Sabino <aguirresabino187@gmail.com>
2019-09-06 20:40:22 -0300

    renomeando variável de ambiente MONGODB_URL para MONGODB_URI

--- server/config/db/mongoConnection.js
+++ server/config/db/mongoConnection.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const OPTIONS_MONGODB = require('./optionsMongoDB');
 
-const { MONGODB_URL, NODE_ENV } = process.env;
+const { MONGODB_URI, NODE_ENV } = process.env;
 
 let singleConnection;
 
 module.exports = () => {
   if (!singleConnection) {
     console.log('CONEXÃO COM O MONGODB SERÁ CRIADA');
-    singleConnection = mongoose.createConnection(MONGODB_URL, OPTIONS_MONGODB[NODE_ENV]);
+    singleConnection = mongoose.createConnection(MONGODB_URI, OPTIONS_MONGODB[NODE_ENV]);
   }
 
   mongoose.connection.on('connected', () => {
